98755fc206c6aee8acdbc9f7092e39519c818a8e
Glen Simon <glen.a.simon@gmail.com>
2023-09-18 07:53:39 -0400

    Update photos

--- src/Components/ImageDisplay.tsx
+++ src/Components/ImageDisplay.tsx
@@ -7,49 +7,52 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const images = [
+interface Image {
+   thumbnail: {
+      uri: string;
+      name?: string;
+   };
+}
+
+const images: Image[] = [
    {
       thumbnail: {
-         uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/800px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg",
+         uri: "https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/352494183_238825822238046_930578791629914522_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5614bc&_nc_ohc=d6m63cMVC1IAX-8Kl8h&_nc_ht=scontent-ord5-1.xx&oh=00_AfAE03wX7ihcF4o6YUvcGioBRCFhKi2zHFjJKAZ-H3UfiQ&oe=650DEA75",
       },
    },
    {
       thumbnail: {
-         uri: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+         uri: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/375858032_283849461069015_2719873419760268979_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=4P6Cvz1hD9wAX9mM0i1&_nc_ht=scontent-ord5-2.xx&oh=00_AfA67vDANCuMEu4sz0BH-z-GvNWTuualY5dHbFdeNeF5sQ&oe=650DCE55",
       },
    },
    {
       thumbnail: {
-         uri: "https://img.freepik.com/free-photo/view-new-york-city-manhattan-midtown-dusk-with-skyscrapers-illuminated-east-river_268835-754.jpg",
+         uri: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/362298710_261025063351455_1552751918041987004_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5614bc&_nc_ohc=LfU8tAA15UoAX8oEmi8&_nc_ht=scontent-ord5-2.xx&oh=00_AfBYnon-Y1EinsS0vcgHIHkbnhfSmOBbP3mXqeoLzRONSg&oe=650E0202",
       },
    },
    {
       thumbnail: {
-         uri: "https://img.freepik.com/free-photo/view-new-york-city-manhattan-midtown-dusk-with-skyscrapers-illuminated-east-river_268835-754.jpg",
+         uri: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/352713793_238437292276899_2783470173549439343_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5614bc&_nc_ohc=4fIT_PxS95cAX8Zi45W&_nc_ht=scontent-ord5-2.xx&oh=00_AfBAyaISnqfbcAEDHqDTSmtfm-bpSOQybacb8k63Xx2vEQ&oe=650D2EA0",
       },
    },
    {
       thumbnail: {
-         uri: "https://cdn.pixabay.com/photo/2017/08/01/20/06/city-2567670_640.jpg",
-         name: "nature",
+         uri: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/353055493_238437238943571_4074894969683806361_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5614bc&_nc_ohc=LZIiloKx3oEAX9qQAvL&_nc_ht=scontent-ord5-2.xx&oh=00_AfDFLPpv1QHtOf6a4HhJoNcQH8aUc2oJtD0PSq9YNQnEKA&oe=650D6E84",
       },
    },
    {
       thumbnail: {
-         uri: "https://media.gettyimages.com/id/1207663571/photo/empty-pavement-with-modern-architecture.jpg?s=612x612&w=gi&k=20&c=HOXes9ocKgFP2JGi8XGHz2SRs3Rrvmyu2Q6UQ8e5bI4=",
-         name: "cats",
+         uri: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/352523257_6271260842929795_2959790090676808907_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5614bc&_nc_ohc=eJfp90EE-VYAX_Ol6gi&_nc_oc=AQnEIbi_50EYQNBqS5BKZPQMZaCEYtccjM8kAAOjl1Gf3R96J9ugJQoyrOYDQ7hhThk&_nc_ht=scontent-ord5-2.xx&oh=00_AfDWTINsUCJtxyf6gGrjVKvXKaBP1QABh9oSobINdrkBig&oe=650CB30A",
       },
    },
    {
       thumbnail: {
-         uri: "https://media.istockphoto.com/id/615398376/photo/new-york-city-nyc-usa.jpg?s=612x612&w=0&k=20&c=rlrsrt4jbORPDSOW5df06Ik_X_5iQo1rYQd53xSs4nw=",
-         name: "cats",
+         uri: "https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/347233033_198013793150343_5885461333045493659_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5614bc&_nc_ohc=puLI7Ejuby8AX9GLxU4&_nc_oc=AQl9IaMqpiFjmhcFlqVL9wT68BimuYU1ZVFe-wgUGNhfg0RM8SCjlM2aqAO0FWXNYpQ&_nc_ht=scontent-ord5-1.xx&oh=00_AfCHgXVT4W0lFQ9hbYZqk3fLAwGqQs6ncnPaTxueOdY7OA&oe=650D23B5",
       },
    },
    {
       thumbnail: {
-         uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPhMWfMxhTNT9dDN2qpGNUJtQgTURtpCJr6w&usqp=CAU",
-         name: "cats",
+         uri: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/335123048_581665440559111_6803252159337314221_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5614bc&_nc_ohc=iAmdXnxenWcAX_Yi1nR&_nc_ht=scontent-ord5-2.xx&oh=00_AfBD1-oWPV0GXKbE35fhNp9FwWmAA38J3ckLfqEGPZ4rGg&oe=650CDAA7",
       },
    },
 ];
@@ -70,11 +73,11 @@ const ImageDisplay: React.FC = () => {
       return 2;
    };
 
-   // todo - figure out better solution for image full screen 
+   // todo - figure out better solution for image full screen
 
    return (
       <Box>
-         <ImageList variant="masonry" cols={getColumnNumber()} gap={8}>
+         <ImageList variant="quilted" cols={getColumnNumber()} gap={8}>
             {images.map((image, index) => (
                <ImageListItem key={index}>
                   <img
