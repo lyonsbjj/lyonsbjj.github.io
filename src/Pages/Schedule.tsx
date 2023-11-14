e29a4eb434d706e9c74c4803bdcd61648aea102e
Glen Simon <glen.a.simon@gmail.com>
2023-11-14 12:26:18 -0500

    Add photos to public folder

--- src/Pages/Schedule.tsx
+++ src/Pages/Schedule.tsx
@@ -4,8 +4,7 @@ import FullScreenImageModal from "../Components/FullScreenImageModal";
 
 const Schedule: React.FC = () => {
    const [showModal, setShowModal] = useState(false);
-   const scheduleLink =
-      "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/353033948_235072785946683_3436439294754026734_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5614bc&_nc_ohc=9VEn9F2P4xMAX_21sKb&_nc_ht=scontent-ord5-2.xx&oh=00_AfAT-qaQjWtzz0o_UP_759-unQvzyT2ejgquKq7yfOVNVA&oe=650D7879";
+   const scheduleLink = "/assets/schedule.jpg";
 
    return (
       <Box sx={{ display: "flex", justifyContent: "center" }}>
